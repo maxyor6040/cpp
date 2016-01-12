3ae3cd3dcac0f6e719bb78df1e9644a6889d8404
Max yor <maxyor6040@gmail.com>
2016-01-12 20:50:23 +0200

    Fields.cpp added
    fields is done ( I think )

---
+++ Field.cpp
@@ -0,0 +1,25 @@
+
+using namespace std;
+#include <iostream>
+#include "Field.h"
+
+Field::Field(std::string fieldName, std::string className):fieldName(fieldName), className(className) {
+    intValue=0;
+    type=INT;
+}
+
+void Field::setType(Type t) { type = t; }
+
+Type Field::getType(){ return type;}
+
+void Field::setInt(Object *obj, int value) { obj->setInt(fieldName,value); }
+
+int Field::getInt(Object* obj){ obj->getInt(fieldName); }
+
+void Field::setObj(Object* obj, Object* value){ obj->setObj(fieldName, value); }
+
+Object* Field::getObj(Object* obj){ obj->getObj(fieldName); }
+
+std::string Field::getDeclaringClass() const{ return std::string(className); }
+
+std::string Field::name() const { return std::string(className); }
